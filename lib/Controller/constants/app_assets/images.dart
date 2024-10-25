e19a32f738aaecd06fb31336ab64f33593325b83
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-10-24 23:41:11 -0700

    update

---
+++ lib/Controller/constants/app_assets/images.dart
@@ -0,0 +1,11 @@
+
+
+class AppImages
+{
+  static String userImage='lib/Controller/assets/user6.png';
+
+
+  static String imgpath='lib/Controller/assets/appimages';
+  static String homeImage='$imgpath/1.jpg';
+  static String profileImge='$imgpath/2.jpg';
+}
