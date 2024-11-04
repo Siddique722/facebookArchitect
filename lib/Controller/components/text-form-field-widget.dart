53e5fb414a2f587be3d8313d1b218ac7f0b55354
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-11-03 22:38:44 -0800

    update code

--- lib/Controller/components/text-form-field-widget.dart
+++ lib/Controller/components/text-form-field-widget.dart
@@ -33,3 +33,41 @@ class TextFormWidget extends StatelessWidget {
     ;
   }
 }
+
+//-----------
+
+class TextFormWidgetUpdate extends StatelessWidget {
+  String hintText;
+  TextEditingController controller;
+  Widget? icon;
+  TextFormWidgetUpdate({super.key,
+    required this.hintText,
+     this.icon,
+    required this.controller
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return     Container(
+      margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+      decoration: BoxDecoration(
+          color: AppColors.appPrimaryColor,
+          borderRadius: BorderRadius.circular(5)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Colors.white),
+              prefixIcon: icon??null
+          ),
+        ),
+      ),
+    )
+    ;
+  }
+}
--- lib/View/home_screens/home-screen.dart
+++ lib/View/home_screens/home-screen.dart
@@ -1,5 +1,8 @@
+
 import 'package:archi/Controller/constants/colors/app-colors.dart';
 import 'package:archi/View/auth_screens/login-screen.dart';
+import 'package:archi/View/home_screens/insertDaraScreen/insert-data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,22 +13,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-
-      },backgroundColor: AppColors.appPrimaryColor,child: Icon(Icons.add,color: Colors.white,),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              CupertinoDialogRoute(
+                  builder: (context) => InsertData(), context: context));
+        },
+        backgroundColor: AppColors.appPrimaryColor,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: AppColors.appPrimaryColor,
         actions: [
-          IconButton(onPressed: ()async{
-            await FirebaseAuth.instance.signOut();
-            Navigator.pushReplacement(context,
-                CupertinoDialogRoute(builder: (context)=>LoginSCreen(),
-                  context: context
-
-                ));
-          }, icon: Icon(Icons.logout,color: Colors.white,))
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
+                    context,
+                    CupertinoDialogRoute(
+                        builder: (context) => LoginSCreen(), context: context));
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
         ],
       ),
+      body: StreamBuilder(
+        // snapshot
+        stream: FirebaseFirestore.instance.collection('Todo').snapshots(),
+        builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
+         final data =snapshot.requireData;
+         return ListView.builder(
+           itemCount: data.size,
+           itemBuilder: (context,index){
+             return  Card(
+               color: AppColors.appPrimaryColor,
+               child: ListTile(
+                 title: Text(data.docs[index]['title']),
+                 subtitle: Text(data.docs[index]['description']),
+               ),
+             );
+           },
+
+         );
+        },
+      )
+
     );
   }
 }
