940cb7ed82bd48a6e4bb0d0c2ffb0fe8ba1e737e
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-11-03 21:17:33 -0800

    update

--- lib/View/home_screens/home-screen.dart
+++ lib/View/home_screens/home-screen.dart
@@ -1,3 +1,7 @@
+import 'package:archi/Controller/constants/colors/app-colors.dart';
+import 'package:archi/View/auth_screens/login-screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +9,23 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+      },backgroundColor: AppColors.appPrimaryColor,child: Icon(Icons.add,color: Colors.white,),),
+      appBar: AppBar(
+        backgroundColor: AppColors.appPrimaryColor,
+        actions: [
+          IconButton(onPressed: ()async{
+            await FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(context,
+                CupertinoDialogRoute(builder: (context)=>LoginSCreen(),
+                  context: context
+
+                ));
+          }, icon: Icon(Icons.logout,color: Colors.white,))
+        ],
+      ),
+    );
   }
 }
--- lib/View/startingScreens/splash-screen.dart
+++ lib/View/startingScreens/splash-screen.dart
@@ -3,6 +3,8 @@ import 'package:archi/Controller/components/primary-text-component.dart';
 import 'package:archi/Controller/constants/app_assets/images.dart';
 import 'package:archi/Controller/constants/colors/app-colors.dart';
 import 'package:archi/View/auth_screens/sign-screen.dart';
+import 'package:archi/View/home_screens/home-screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,11 +21,27 @@ class _SpalshScreenState extends State<SpalshScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 3,),(){
-      Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context)=>RegistraytionScreen()));
+    Future.delayed(Duration(seconds: 3,),()
+    {
+      isLogedin();
     });
   }
 
+
+  //-----
+isLogedin()
+{
+  User? user=FirebaseAuth.instance.currentUser;
+  if(user==null)
+    {
+      Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context)=>RegistraytionScreen()));
+
+    }
+  else
+    {
+      Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context)=>HomeScreen()));
+    }
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
